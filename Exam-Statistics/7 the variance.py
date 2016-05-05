93b6416fa3418ad5f68506d835dfd9d4480e94ee
nam-lah <nam-lah@users.noreply.github.com>
2016-05-04 19:16:18 -0600

    Update 7 the variance.py

--- Exam-Statistics/7 the variance.py
+++ Exam-Statistics/7 the variance.py
@@ -12,16 +12,13 @@ def grades_sum(grades):
     
 def grades_average(grades):
     sum_of_grades = grades_sum(grades)
-    average = sum_of_grades / float(len(grades))
-    return average
+    average =sum_of_grades / float(len(grades))
+    return float(average)
 
 def grades_variance(scores):
-    average = grades_average(scores)
-    variance = 0
-    result = 0
-    for x in scores:
-        variance += (average - x) ** 2
+    average = grades_average(scores); variance = 0
+    for score in scores:
+        variance += ((average - score) ** 2)
     return variance / float(len(scores))
-    
 
 print grades_variance(grades)
