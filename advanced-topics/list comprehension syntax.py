15db9411cc42d9be064f4cab5fb553ac2d911fff
nam-lah <nam-lah@users.noreply.github.com>
2016-05-07 19:00:38 -0600

    Update list comprehension syntax.py

--- advanced-topics/list comprehension syntax.py
+++ advanced-topics/list comprehension syntax.py
@@ -1,1 +1,6 @@
+
+# square list for even numbers only in a range of 1-12
 [x**2 for x in range(1,12) if (x) % 2 == 0]
+
+# list of doubles for numbers divisible by three, range of 1-6
+[x*2 for x in range(1,6) if (x*2) % 3 == 0]
