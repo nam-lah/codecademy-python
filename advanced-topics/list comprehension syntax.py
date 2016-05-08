736da7e3092ca1a0932d35c91d0c50dc9e064951
nam-lah <nam-lah@users.noreply.github.com>
2016-05-07 19:05:54 -0600

    Update list comprehension syntax.py

--- advanced-topics/list comprehension syntax.py
+++ advanced-topics/list comprehension syntax.py
@@ -8,3 +8,11 @@
 # other comprehension list
 cubes_by_four = [x**3 for x in range(1,11) if (x**3) % 4 ==0]
 print cubes_by_four
+
+
+# list slicing
+l = [i ** 2 for i in range(1, 11)]
+# will print  [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+
+print l[2:9:2]
+# will print [9, 25, 49, 81]
