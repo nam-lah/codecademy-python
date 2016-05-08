2280938d038f3199f0ac7f332c433e0921e44d79
nam-lah <nam-lah@users.noreply.github.com>
2016-05-07 18:49:25 -0600

    Create building lists.py

---
+++ advanced-topics/building lists.py
@@ -0,0 +1,12 @@
+evens_to_50 = [i for i in range(51) if i % 2 == 0]
+print evens_to_50
+
+odds = [i for i in range(52) if i % 2 != 0]
+print odds
+
+for x in range(200, 305, 3):
+    print x
+    
+
+test = [x for x in range(197, 310, 3)]
+print test
