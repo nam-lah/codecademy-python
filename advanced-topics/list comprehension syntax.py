7fa3e7d302a92a3b4b507de1ffd66287c2ef198a
nam-lah <nam-lah@users.noreply.github.com>
2016-05-07 19:03:58 -0600

    Update list comprehension syntax.py

--- advanced-topics/list comprehension syntax.py
+++ advanced-topics/list comprehension syntax.py
@@ -4,3 +4,7 @@
 
 # list of doubles for numbers divisible by three, range of 1-6
 [x*2 for x in range(1,6) if (x*2) % 3 == 0]
+
+# other comprehension list
+cubes_by_four = [x**3 for x in range(1,11) if (x**3) % 4 ==0]
+print cubes_by_four
